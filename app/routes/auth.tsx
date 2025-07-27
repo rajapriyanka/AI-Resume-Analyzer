412e200792cc796e47e3c1802cfea33bfe07ef90
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-07-27 08:09:38 +0530

    Update auth.tsx

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -2,6 +2,7 @@ import {usePuterStore} from "~/lib/puter";
 import {useEffect} from "react";
 import {useLocation, useNavigate} from "react-router";
 
+
 export const meta = () => ([
     { title: 'Resumind | Auth' },
     { name: 'description', content: 'Log into your account' },
